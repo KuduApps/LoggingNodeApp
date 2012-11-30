dbdb84024204e12984a8b9434e5dec93ba739323
dravva <dravva@microsoft.com>
2012-11-30 11:07:34 -0800

    temp commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    console.log('writing response header');
+    console.log('writing response headers');
     res.writeHead(200, {'Content-Type': 'text/html'});
    console.log('writing response');
     res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']');
